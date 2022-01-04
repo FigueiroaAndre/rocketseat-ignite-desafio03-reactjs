2b4f7feea23dfe05d4179d5e8eeb3858e48639a6
Andre Figueiroa <alfigueiroab@gmail.com>
2022-01-03 22:25:33 -0300

    Implement 'add to cart' feature at Home page

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -34,9 +34,47 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const productInCart = cart.find(product => product.id === productId);
+      const { data: productStock } = await api.get<Stock>(`/stock/${productId}`);
+      if (productStock.amount <= 0) {
+        toast.error('Quantidade solicitada fora de estoque');
+        return;
+      }
+
+      if (productInCart) {
+        const hasStockAvailable = productStock.amount > productInCart.amount;
+        if (!hasStockAvailable) {
+          toast.error('Quantidade solicitada fora de estoque');
+          return;
+        }
+        setCart(cart => {
+          const updatedCart = cart.map(product => {
+            if (product.id === productId) {
+              return {
+                ...product,
+                amount: product.amount + 1
+              }
+            }
+            return product;
+          });
+          localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
+          return updatedCart;
+        })
+      }
+      else {
+        const { data } = await api.get<Product>(`/products/${productId}`);
+        const newProduct: Product = {
+          ...data,
+          amount: 1,
+        }
+        setCart(cart => {
+          const updatedCart = [...cart, newProduct];
+          localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
+          return updatedCart;
+        });
+      }
     } catch {
-      // TODO
+      toast.error('Erro na adição do produto');
     }
   };
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -47,7 +47,7 @@ const Home = (): JSX.Element => {
   }, []);
 
   function handleAddProduct(id: number) {
-    // TODO
+    addProduct(id);
   }
 
   return (
@@ -60,7 +60,7 @@ const Home = (): JSX.Element => {
         <button
           type="button"
           data-testid="add-product-button"
-        // onClick={() => handleAddProduct(product.id)}
+          onClick={() => handleAddProduct(product.id)}
         >
           <div data-testid="cart-product-quantity">
             <MdAddShoppingCart size={16} color="#FFF" />
